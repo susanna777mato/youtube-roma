470ea3c85d6c83e689b94c54e72fa0da4efc8c15
susanna777mato <susanna.mato777@gmail.com>
2024-10-03 00:28:44 -0700

    i make 2 section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
